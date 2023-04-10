d5129ce82c26a33f29f906f17511ca978e77bbb3
Xingbin Gao <xingbingao@gmail.com>
2023-04-10 22:06:26 +1200

    Commit 3

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,13 @@
+const { inspect } = require('util');
 const functions = require('@google-cloud/functions-framework');
 
 const connectMongoDB = require('./app/common/mongo_conn');
+const app = require('./app');
 
-functions.http('api', (req, res) => {
-  res.send('OK or Not');
+functions.http('api', async (req, res) => {
+  return await connectMongoDB().then(() => {
+    return app(req, res);
+  });
 });
 
 functions.http('cron', (req, res) => {
